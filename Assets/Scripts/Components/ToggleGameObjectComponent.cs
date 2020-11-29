fb6032a7c2d364c28345e4a8bcd2f2574b7f4e3a
Gabriel Tupy <gabetupy@gmail.com>
2020-11-28 20:53:08 -0600

    Added a new sprite, may redo some of the sprite work

---
+++ Assets/Scripts/Components/ToggleGameObjectComponent.cs
@@ -0,0 +1,26 @@
+﻿//Written By Gabriel Tupy on 11-23-20
+//Last Modified by Gabriel Tupy on 11-28-20
+
+using UnityEngine;
+
+/// <summary>
+/// A script that allows you to disable/enable a set gameobject.
+/// </summary>
+public class ToggleGameObjectComponent : MonoBehaviour
+{
+    //Stored Variables
+    #region
+    [Tooltip("The GameObject to be Toggled.")] [SerializeField] private GameObject toggleObject = null;
+    #endregion
+
+    /// <summary>
+    /// Toggles the ToggleObject.
+    /// </summary>
+    public void ToggleObject()
+    {
+        if (toggleObject != null)
+        {
+            toggleObject.SetActive(!toggleObject.activeSelf);
+        }
+    }
+}
